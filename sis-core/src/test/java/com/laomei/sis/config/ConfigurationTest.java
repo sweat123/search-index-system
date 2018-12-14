4f26e7f786ac82aa81c7244ce2d3627c0cb4726a
luobo <1208878425@qq.com>
2018-12-14 19:47:56 +0800

    refact pom

---
+++ sis-core/src/test/java/com/laomei/sis/config/ConfigurationTest.java
@@ -0,0 +1,15 @@
+package com.laomei.sis.config;
+
+import org.junit.Test;
+
+/**
+ * @author laomei on 2018/12/14 19:22
+ */
+public class ConfigurationTest {
+
+    @Test
+    public void test() {
+        //todo base configuration test
+        System.out.println("hello");
+    }
+}
