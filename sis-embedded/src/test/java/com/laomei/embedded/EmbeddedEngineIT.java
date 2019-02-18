5cc740cfc659abccbdbe45da11d7bfd7692743a6
sweat123 <sweatott@gmail.com>
2019-02-18 22:36:48 +0800

    add kafka init unit test

--- sis-embedded/src/test/java/com/laomei/embedded/EmbeddedEngineIT.java
+++ sis-embedded/src/test/java/com/laomei/embedded/EmbeddedEngineIT.java
@@ -1,42 +1,39 @@
-/*
- * EmbeddedEngineIT.java
- * Copyright 2019 Qunhe Tech, all rights reserved.
- * Qunhe PROPRIETARY/CONFIDENTIAL, any form of usage is subject to approval.
- */
-
 package com.laomei.embedded;
 
 import org.apache.avro.generic.GenericRecord;
+import org.apache.commons.compress.utils.IOUtils;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.util.StringUtils;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 /**
- * @author luobo.hwz on 2019/2/17 15:22
+ * @author laomei on 2019/2/17 15:22
  */
 public class EmbeddedEngineIT {
 
     private JdbcTemplate jdbcTemplate;
 
     @Before
-    public void init() {
+    public void init() throws IOException, InterruptedException {
+        initDbzTask();
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("com.mysql.jdbc.Driver");
         dataSource.setUrl("jdbc:mysql://localhost:13306/sis?useSSL=false");
         dataSource.setUsername("sis-user");
         dataSource.setPassword("sis-password");
         jdbcTemplate = new JdbcTemplate(dataSource);
-
-        initDbzTask();
     }
 
     @Test
@@ -61,12 +58,37 @@ public class EmbeddedEngineIT {
         config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "io.confluent.kafka.serializers.KafkaAvroDeserializer");
         config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "io.confluent.kafka.serializers.KafkaAvroDeserializer");
-        config.put("schema-registry-url", "127.0.0.1");
-//        KafkaConsumer<GenericRecord, GenericRecord> consumer = new KafkaConsumer<>(config);
-//        consumer.subscribe("");
+        config.put("schema-registry-url", "127.0.0.1:8082");
+        KafkaConsumer<GenericRecord, GenericRecord> consumer = new KafkaConsumer<>(config);
+        consumer.subscribe(Collections.singletonList("sis.sis.user_desc"));
+
+        ConsumerRecords<GenericRecord, GenericRecord> records = consumer.poll(3000);
+        // there will be 3 records in kafka topic;
+        int cnt = records.count();
+        Assert.assertEquals(cnt, 3);
+
+        jdbcTemplate.execute("UPDATE user_desc SET address = 'newAddress' WHERE id = 1");
+
+        records = consumer.poll(3000);
+        Assert.assertEquals(records.count(), 1);
+        ConsumerRecord<GenericRecord, GenericRecord> record = records.iterator().next();
+        String address = String.valueOf(record.value().get("address"));
+        Assert.assertEquals("newAddress", address);
     }
 
-    private void initDbzTask() {
+    private void initDbzTask() throws IOException, InterruptedException {
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        String config = null;
+        try (InputStream in = classLoader.getResourceAsStream("dbz/connect.json")) {
+            byte[] bytes = IOUtils.toByteArray(in);
+            config = new String(bytes, "UTF-8");
+        }
+        if (StringUtils.isEmpty(config)) {
+            throw new IllegalStateException("dbz config can not be blank");
+        }
+        HttpUtil.doPost("localhost:8083/connectors", config);
 
+        //sleep 5s to make sure dbz task has started;
+        TimeUnit.SECONDS.sleep(5);
     }
 }
---
+++ sis-embedded/src/test/java/com/laomei/embedded/HttpUtil.java
@@ -0,0 +1,35 @@
+package com.laomei.embedded;
+
+import okhttp3.*;
+
+import java.io.IOException;
+
+/**
+ * @author laomei on 2018/10/24 15:02
+ */
+public class HttpUtil {
+
+    private static final OkHttpClient CLIENT = new OkHttpClient();
+
+    private static final MediaType DEFAULT_MEDIA_TYPE = MediaType.parse("application/json; charset=utf-8");
+
+    public static void doPost(String url, String body) throws IOException {
+
+        RequestBody requestBody = RequestBody.create(DEFAULT_MEDIA_TYPE, body);
+
+        //create header
+        Headers.Builder headersBuilder = new Headers.Builder();
+        headersBuilder.add("Content-Type", "application/json");
+        Headers headers = headersBuilder.build();
+
+        //create request
+        Request request = new Request.Builder()
+                .url(url)
+                .headers(headers)
+                .post(requestBody)
+                .build();
+
+        //exec request
+        CLIENT.newCall(request).execute();
+    }
+}
